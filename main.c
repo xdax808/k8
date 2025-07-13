2c83470f7aa0678a710c3a1c53687e21a9ec2b96
xdax808 <baseman@web.de>
2025-07-13 10:53:19 +0200

    changed to vga

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 
 #include <stdint-gcc.h>
 
-
-void __attribute__((naked))_main()
+void __attribute__((naked))kernel_main()
 {
-    volatile uint16_t* p = (uint16_t*)0xe0000000;
+    // volatile uint16_t* p = (uint16_t*)0xe0000000;
+    volatile uint16_t* p = (uint16_t*)0xb8000;
 
     while(1) {
         (*p)++;
