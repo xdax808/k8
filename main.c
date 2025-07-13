44084edc46a7b439ae4028f0148ad1ef5bce682c
xdax808 <baseman@web.de>
2025-07-13 11:51:11 +0200

    testing

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 
 #include <stdint-gcc.h>
 
-void __attribute__((naked))kernel_main()
+void kernel_main()__attribute__ ((section (".text.init")));
+int  tst(int a, int b, int c);
+
+
+// void __attribute__((naked))kernel_main()
+void kernel_main()
 {
     // volatile uint16_t* p = (uint16_t*)0xe0000000;
     volatile uint16_t* p = (uint16_t*)0xb8000;
 
     while(1) {
         (*p)++;
+        int q = tst(*(p+1),*(p+2),*p);
+        *p = q;
+        // p++;
     }
+}
+
+int __attribute__((noinline))tst(int a, int b, int c)
+{
+   return a+b+c;
 }
